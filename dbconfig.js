2a0a003e9e611e3cb11f8b20a575fb5753ffbc1f
shubhkumar241-sk <shubhkumar241@gmail.com>
2025-11-01 12:27:42 +0530

    Add files via upload

---
+++ dbconfig.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL database");
+});
+
+module.exports = pool;
