678fa66659da8255955565e167bc19484e327273
shubhkumar241-sk <shubhkumar241@gmail.com>
2025-11-01 23:40:29 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 
 function apiKeyAuth(req, res, next) {
   const apiKey = req.header("x-api-key");
-  console.log("Received Key:", apiKey);
-  console.log("Env Key:", process.env.API_KEY);
+
 
   if (!apiKey || apiKey !== process.env.API_KEY) {
     return res.status(401).json({ error: "Unauthorized: Invalid API Key" });
